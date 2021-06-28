2e3fcf0f21ca570e722237c5f9fab3fab604dbb5
Brayon <braydonalan@gmail.com>
2021-06-28 16:14:52 -0400

    Reorganized Tests

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function it_tests_something()
+    {
+        $this->assertTrue(true);
+    }
+
+}
