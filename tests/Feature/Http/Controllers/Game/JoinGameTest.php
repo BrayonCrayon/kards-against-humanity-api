5d2116446cc8b39762b39fa28ffea31850cbc97c
bradyderoy <oR57$!^6yZbx>
2021-08-04 15:56:11 -0400

    Ensure white cards come from selected expansion

--- tests/Feature/Http/Controllers/Game/JoinGameTest.php
+++ tests/Feature/Http/Controllers/Game/JoinGameTest.php
@@ -14,17 +14,19 @@ use Tests\TestCase;
 class JoinGameTest extends TestCase
 {
     private $game;
+    private $expansionIds;
     protected function setUp(): void
     {
         parent::setUp();
-        $expansionIds = Expansion::all()->pluck('id')->toArray();
+        $this->expansionIds = Expansion::where('id', 1)
+            ->pluck('id')->toArray();
         $user = User::factory()->create();
         $this->game = Game::factory()->create();
         $this->game->users()->save($user);
-        $this->game->expansions()->saveMany(Expansion::idsIn($expansionIds)->get());
+        $this->game->expansions()->saveMany(Expansion::idsIn($this->expansionIds)->get());
 
         $gameService = new GameService();
-        $gameService->grabBlackCards($user, $this->game, $expansionIds);
+        $gameService->grabBlackCards($user, $this->game, $this->expansionIds);
     }
 
     /** @test */
@@ -44,4 +46,20 @@ class JoinGameTest extends TestCase
         ])->assertOK();
         $this->assertCount(Game::HAND_LIMIT, User::find($joinGameResponse['user']['id'])->whiteCards);
     }
+
+    /** @test */
+    public function it_brings_back_white_cards_that_are_in_specific_expansions()
+    {
+        $joinGameResponse = $this->postJson(route('api.game.join', $this->game->id), [
+            'userName' => $this->faker->userName
+        ])->assertOK();
+
+        $currentExpansionIds = User::find($joinGameResponse['user']['id'])->whiteCards->pluck('expansion_id');
+
+        // assert that the users white cards has an expansion id (pluck them out) - all of them are found in the expansion ids array
+        $currentExpansionIds->each(function ($id) {
+            $this->assertContains($id, $this->expansionIds);
+        });
+    }
+
 }
