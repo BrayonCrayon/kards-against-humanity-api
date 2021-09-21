132970c1846ee1b62bf5605e2166ce1f284a775e
bradyderoy <oR57$!^6yZbx>
2021-09-21 15:41:58 -0400

    WIP
    # Conflicts:
    #	composer.lock

---
+++ app/Console/Commands/DummyCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Events\DummyEvent;
+use Illuminate\Console\Command;
+
+class DummyCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dummy:broadcast';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+//        event(DummyEvent::class);
+        DummyEvent::dispatch();
+        return 0;
+    }
+}
