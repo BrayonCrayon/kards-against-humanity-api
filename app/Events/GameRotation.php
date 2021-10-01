c4c7e6e7c8c6e5e07a5e61d311b0e05cda455378
bradyderoy <oR57$!^6yZbx>
2021-10-01 15:17:13 -0400

    Refactor

--- app/Events/GameRotation.php
+++ app/Events/GameRotation.php
@@ -2,16 +2,13 @@
 
 namespace App\Events;
 
-use App\Models\WhiteCard;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class GameRotation implements ShouldBroadcast
+class GameRotation implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
