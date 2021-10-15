5547a41b9d03b2d9dab2b74e03b68e04da24133b
bradyderoy <oR57$!^6yZbx>
2021-10-15 14:54:30 -0400

    Add id to ExpansionResource

--- app/Http/Resources/Game/ExpansionResource.php
+++ app/Http/Resources/Game/ExpansionResource.php
@@ -15,6 +15,7 @@ class ExpansionResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->resource->id,
             'name' => $this->resource->name,
             'created_at' => $this->resource->created_at,
             'updated_at' => $this->resource->updated_at,
