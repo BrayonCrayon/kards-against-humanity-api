8d1510832909b59e9c17ea2127381c6e7a824979
bradyderoy <oR57$!^6yZbx>
2021-12-11 12:51:50 -0500

    refactor

--- tests/Feature/Models/User/UserTest.php
+++ tests/Feature/Models/User/UserTest.php
@@ -11,11 +11,18 @@ use Tests\TestCase;
 class UserTest extends TestCase
 {
     private $gameService;
+    private $user;
+    private $game;
 
     protected function setUp(): void
     {
         parent::setUp();
         $this->gameService = new GameService();
+        $this->user = User::factory()->create();
+        $this->actingAs($this->user);
+        $this->game = $this->gameService->createGame($this->user, Expansion::all()->pluck('id'));
+
+        $this->gameService->drawWhiteCards($this->user, $this->game);
     }
 
     /** @test */
@@ -30,51 +37,27 @@ class UserTest extends TestCase
     /** @test */
     public function has_submitted_white_cards_attribute_brings_back_true_if_user_has_submitted_cards()
     {
-        $user = User::factory()->create();
-        $this->actingAs($user);
-        $game = $this->gameService->createGame($user, Expansion::all()->pluck('id'));
-
-        $this->gameService->drawWhiteCards($user, $game);
-        $this->gameService->submitCards($user->whiteCards->take(2)->pluck('id'),$game);
-        $this->assertTrue($user->hasSubmittedWhiteCards);
+        $this->gameService->submitCards($this->user->whiteCards->take(2)->pluck('id'), $this->game);
+        $this->assertTrue($this->user->hasSubmittedWhiteCards);
     }
 
     /** @test */
     public function has_submitted_white_cards_attribute_returns_false_when_no_cards_are_submitted()
     {
-        $user = User::factory()->create();
-        $this->actingAs($user);
-        $game = $this->gameService->createGame($user, Expansion::all()->pluck('id'));
-
-        $this->gameService->drawWhiteCards($user, $game);
-        $this->assertFalse($user->hasSubmittedWhiteCards);
+        $this->assertFalse($this->user->hasSubmittedWhiteCards);
     }
 
     /** @test */
     public function it_returns_submitted_white_card_ids_when_user_has_submitted_cards()
     {
-        $user = User::factory()->create();
-        $this->actingAs($user);
-
-        $game = $this->gameService->createGame($user, Expansion::all()->pluck('id'));
-
-        $this->gameService->drawWhiteCards($user, $game);
-
-        $submittedWhiteCardIds = $user->whiteCards->take(2)->pluck('id');
-        $this->gameService->submitCards($submittedWhiteCardIds,$game);
-        $submittedWhiteCardIds->each(fn ($cardId) => $this->assertContains($cardId, $user->submittedWhiteCardIds));
+        $submittedWhiteCardIds = $this->user->whiteCards->take(2)->pluck('id');
+        $this->gameService->submitCards($submittedWhiteCardIds, $this->game);
+        $submittedWhiteCardIds->each(fn ($cardId) => $this->assertContains($cardId, $this->user->submittedWhiteCardIds));
     }
 
     /** @test */
     public function submitted_white_cards_returns_empty_array_when_there_are_no_submitted_cards()
     {
-        $user = User::factory()->create();
-        $this->actingAs($user);
-
-        $game = $this->gameService->createGame($user, Expansion::all()->pluck('id'));
-
-        $this->gameService->drawWhiteCards($user, $game);
-
-        $this->assertEmpty($user->submittedWhiteCardIds);
+        $this->assertEmpty($this->user->submittedWhiteCardIds);
     }
 }
