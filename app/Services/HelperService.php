f4607b8e6c2beabdb22623877e05a8fdbe6c05b1
bradyderoy <oR57$!^6yZbx>
2021-12-20 16:28:07 -0500

    Add check functions to determine whether a character is an alpha character for upper or lower Allow code generator to generate alpha codes

--- app/Services/HelperService.php
+++ app/Services/HelperService.php
@@ -4,9 +4,34 @@ namespace App\Services;
 
 class HelperService
 {
+    const UPPER_CASE_ALPHA_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const UPPER_CASE_ALPHA_ASCII_START = 65;
+    const UPPER_CASE_ALPHA_ASCII_END = 90;
+    const LOWER_CASE_ALPHA_CHARACTERS = "abcdefghijklmnopqrstuvwxyz";
+    const LOWER_CASE_ALPHA_ASCII_START = 97;
+    const LOWER_CASE_ALPHA_ASCII_END = 122;
+
+    public function isUpperAlphaCharacter(string $char): bool
+    {
+        return ord($char) >= self::UPPER_CASE_ALPHA_ASCII_START && ord($char) <= self::UPPER_CASE_ALPHA_ASCII_END;
+    }
+
+    public function isLowerAlphaCharacter(string $char): bool
+    {
+        return ord($char) >= self::LOWER_CASE_ALPHA_ASCII_START && ord($char) <= self::LOWER_CASE_ALPHA_ASCII_END;
+    }
 
     public function generateCode(string $codeFormat = ""): string
     {
-        return "";
+        $code = "";
+
+        foreach (str_split($codeFormat) as $item) {
+            if ($item === "#")
+                $code .= '1';
+            else if ($item === "?")
+                $code .= 'A';
+        }
+
+        return $code;
     }
 }
--- tests/Unit/Services/HelperServiceTest.php
+++ tests/Unit/Services/HelperServiceTest.php
@@ -15,10 +15,67 @@ class HelperServiceTest extends TestCase
     }
 
     /** @test */
-    public function it_will_bring_back_empty_code_when_no_format_string_is_given()
+    public function it_will_return_true_when_all_characters_in_string_are_uppercase_alpha_characters()
+    {
+        foreach (str_split(HelperService::UPPER_CASE_ALPHA_CHARACTERS) as $char) {
+            $this->assertTrue($this->service->isUpperAlphaCharacter($char));
+        }
+    }
+
+    /** @test */
+    public function it_will_return_false_when_all_character_is_not_uppercase_alpha_character()
+    {
+        foreach (str_split(HelperService::LOWER_CASE_ALPHA_CHARACTERS) as $char) {
+            $this->assertFalse($this->service->isUpperAlphaCharacter($char));
+        }
+    }
+
+    /** @test */
+    public function it_will_return_true_when_all_characters_in_string_are_lowercase_alpha_characters()
+    {
+        foreach (str_split(HelperService::LOWER_CASE_ALPHA_CHARACTERS) as $char) {
+            $this->assertTrue($this->service->isLowerAlphaCharacter($char));
+        }
+    }
+
+    /** @test */
+    public function it_will_return_false_when_all_characters_in_string_are_uppercase_alpha_characters()
+    {
+        foreach (str_split(HelperService::UPPER_CASE_ALPHA_CHARACTERS) as $char) {
+            $this->assertFalse($this->service->isLowerAlphaCharacter($char));
+        }
+    }
+
+    /** @test */
+    public function it_will_bring_back_empty_string_code_when_no_format_string_is_given()
     {
         $code = $this->service->generateCode();
 
+        $this->assertIsString($code);
         $this->assertEmpty($code);
+    }
+
+    /** @test */
+    public function it_will_bring_back_code_in_digit_format()
+    {
+        $codeFormat = "####";
+        $code = $this->service->generateCode("####");
+
+        $this->assertEquals(strlen($codeFormat), strlen($code));
+        foreach (str_split($code) as $codeChar) {
+            $this->assertIsNumeric($codeChar);
+        }
+    }
+
+    /** @test */
+    public function it_will_bring_back_code_in_character_format()
+    {
+        $codeFormat = "????";
+        $code = $this->service->generateCode($codeFormat);
+
+        $this->assertEquals(strlen($codeFormat), strlen($code));
+        foreach (str_split($code) as $codeChar) {
+            $this->assertTrue($this->service->isUpperAlphaCharacter($codeChar));
+        }
     }
 }
