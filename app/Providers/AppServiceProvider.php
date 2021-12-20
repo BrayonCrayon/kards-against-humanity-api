26837689fba53dde85697028b53bb51623592c52
bradyderoy <oR57$!^6yZbx>
2021-12-20 17:00:00 -0500

    Make helper service a singleton Use helper service generate code to generate game code instead of observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\HelperService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->singleton(HelperService::class, fn () => new HelperService());
     }
 }
