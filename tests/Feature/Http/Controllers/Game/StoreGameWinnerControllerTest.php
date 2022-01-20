949029b830bb4a95e9b659cbe9a557eae49eeef8
bradyderoy <oR57$!^6yZbx>
2022-01-20 16:59:39 -0500

    Add auth/data not found tests

--- tests/Feature/Http/Controllers/Game/StoreGameWinnerControllerTest.php
+++ tests/Feature/Http/Controllers/Game/StoreGameWinnerControllerTest.php
@@ -13,6 +13,24 @@ class StoreGameWinnerControllerTest extends TestCase
 {
 
     /** @test */
+    public function it_does_not_allow_game_that_does_not_exist()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user)->postJson(route('api.game.winner', $this->faker->uuid), [
+            'user_id' => $user->id,
+        ])->assertNotFound();
+    }
+
+    /** @test */
+    public function it_does_not_allow_non_authed_user()
+    {
+        $game = Game::factory()->create();
+        $this->postJson(route('api.game.winner', $game->id), [
+            'user_id' => $game->judge->id,
+        ])->assertUnauthorized();
+    }
+
+    /** @test */
     public function it_stores_game_winner_will_data()
     {
         $game = Game::factory()->hasUsers(1)->create();
