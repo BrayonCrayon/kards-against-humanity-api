268fe97967ddff7a520e9ccd2a7bc6fe401187b7
bradyderoy <oR57$!^6yZbx>
2022-02-05 10:48:12 -0500

    refactor

--- tests/Events/WinnerSelectedTest.php
+++ tests/Events/WinnerSelectedTest.php
@@ -9,41 +9,40 @@ use Tests\TestCase;
 
 class WinnerSelectedTest extends TestCase
 {
+    private $game;
+    private $user;
+    private $event;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->game = Game::factory()->create();
+        $this->user = User::factory()->create();
+
+        $this->event = new WinnerSelected($this->game, $this->user);
+    }
+
     /** @test */
     public function it_sends_user_id_and_game_id_in_event_payload()
     {
-        $game = Game::factory()->create();
-        $user = User::factory()->create();
-
-        $event = new WinnerSelected($game, $user);
-
         $payload = [
-            'game_id' => $game->id,
-            'user_id' => $user->id
+            'game_id' => $this->game->id,
+            'user_id' => $this->user->id
         ];
 
-        $this->assertEquals($payload, $event->broadcastWith());
+        $this->assertEquals($payload, $this->event->broadcastWith());
     }
 
     /** @test */
     public function it_broadcasts_on_correct_channel_name()
     {
-        $game = Game::factory()->create();
-        $user = User::factory()->create();
-
-        $event = new WinnerSelected($game, $user);
-
-        $this->assertEquals("game-{$game->id}", $event->broadcastOn()->name);
+        $this->assertEquals("game-{$this->game->id}", $this->event->broadcastOn()->name);
     }
 
     /** @test */
     public function it_broadcasts_on_correct_event_name()
     {
-        $game = Game::factory()->create();
-        $user = User::factory()->create();
-
-        $event = new WinnerSelected($game, $user);
-
-        $this->assertEquals("winner.selected", $event->broadcastAs());
+        $this->assertEquals("winner.selected", $this->event->broadcastAs());
     }
 }
