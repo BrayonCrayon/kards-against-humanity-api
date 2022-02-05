4d8237fd29c5ff0b93d3cad7842cb307e84f41f5
bradyderoy <oR57$!^6yZbx>
2022-02-05 09:29:12 -0500

    Improve importing cards

--- app/Console/Commands/ImportCards/ImportCards.php
+++ app/Console/Commands/ImportCards/ImportCards.php
@@ -42,7 +42,6 @@ class ImportCards extends Command
     {
         $startLoading = now();
         $this->loadCards();
-//        collect($this->loadDeck())->each(fn ($expansionData) => $this->createExpansion($expansionData));
         $endLoading = now();
         $this->info("Loading Deck: {$endLoading->diffInMilliseconds($startLoading)}}");
         return 0;
@@ -52,58 +51,9 @@ class ImportCards extends Command
     {
         $cardData = $this->loadDecks();
 
-//        $expansions = collect($cardData['expansions'])->map(fn($expansion) => Expansion::make([
-//            'id' => $expansion['id'],
-//            'name' => $expansion['name']
-//        ]))->toArray();
-
-//        $whiteCards = collect($cardData['white_cards'])->map(fn($whiteCard) => WhiteCard::make([
-//            'text' => $whiteCard['text'],
-//            'expansion_id' => $whiteCard['expansion_id']
-//        ]))->toArray();
-
-//        $blackCards = collect($cardData['black_cards'])->map(fn($blackCard) => BlackCard::make([
-//            'text' => $blackCard['text'],
-//            'pick' => $blackCard['pick'],
-//            'expansion_id' => $blackCard['expansion_id']
-//        ]))->toArray();
-
-//        Expansion::insert($expansions);
-//        WhiteCard::insert($whiteCards);
-//        BlackCard::insert($blackCards);
-
         Expansion::insert($cardData['expansions']);
         WhiteCard::insert($cardData['white_cards']);
         BlackCard::insert($cardData['black_cards']);
-    }
-
-    private function createExpansion($expansionData)
-    {
-        $this->info("Importing: {$expansionData['name']}");
-
-        $expansion = Expansion::create([
-            'name' => $expansionData['name']
-        ]);
-
-        $whiteCards = collect($expansionData['white'])->map(fn($whiteCard) => WhiteCard::make([
-            'text' => $whiteCard['text'],
-            'expansion_id' => $expansion->id
-        ]));
-        $blackCards = collect($expansionData['black'])->map(fn($blackCard) => BlackCard::make([
-            'text' => $blackCard['text'],
-            'pick' => $blackCard['pick'],
-            'expansion_id' => $expansion->id
-        ]));
-
-        $this->info("Starting array conversion");
-        $test = $whiteCards->toArray();
-        $test2 = $blackCards->toArray();
-        $this->info("Finished converting white and black cards to arrays");
-        $this->info("starting inserting");
-        WhiteCard::insert($test);
-        $this->info("Finished inserting White cards");
-        BlackCard::insert($test2);
-        $this->info("Finished inserting Black cards");
     }
 
     private function loadDecks()
