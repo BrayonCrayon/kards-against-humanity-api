83c4a5fe8b455a212e504e4abab6b3ecc44b84c5
bradyderoy <braydonalan@gmail.com>
2022-02-12 12:09:47 -0500

    Refactor

--- app/Actions/UserJoinsGame.php
+++ app/Actions/UserJoinsGame.php
@@ -2,10 +2,24 @@
 
 namespace App\Actions;
 
+use App\Models\Game;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
 class UserJoinsGame
 {
-    public function __invoke()
+    public function __invoke(Game $game, $name)
     {
+        if (Auth::check()) {
+            return;
+        }
 
+        $user = User::create([
+            'name' => $name
+        ]);
+        Auth::login($user);
+
+        $this->gameService->drawWhiteCards($user, $game);
+        $this->gameService->joinGame($game, $user);
     }
 }
