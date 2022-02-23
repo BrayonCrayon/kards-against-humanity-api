9ff255f15a9ba8211171c48b22a7d52cade6148a
bradyderoy <braydonalan@gmail.com>
2022-02-22 20:45:00 -0500

    Refactor

--- app/Actions/UserJoinsGame.php
+++ app/Actions/UserJoinsGame.php
@@ -15,13 +15,12 @@ class UserJoinsGame
     public function __invoke(Game $game, string $name)
     {
         $creatingUser = new CreatingUser();
-        $user = null;
-        if (auth()->check() && auth()->user()->games->first()->id !== $game->id) {
-            $user = $creatingUser($name);
-        } else {
-            $user = Auth::check() ? Auth::user() : $creatingUser($name);
+
+        if (Auth::check()) {
+            Auth::logout();
         }
 
+        $user = $creatingUser($name);
 
         $this->service->drawWhiteCards($user, $game);
         $this->service->joinGame($game, $user);
