b5532a29b5b64c96812c012de0f06010f3102fe2
bradyderoy <oR57$!^6yZbx>
2022-03-03 15:46:02 -0500

    Return black card when retrieving the round winner

--- tests/Feature/Http/Controllers/Game/Round/GetRoundWinnerControllerTest.php
+++ tests/Feature/Http/Controllers/Game/Round/GetRoundWinnerControllerTest.php
@@ -59,6 +59,11 @@ class GetRoundWinnerControllerTest extends TestCase
                             'expansion_id',
                             'order',
                         ]
+                    ],
+                    'black_card' => [
+                        'id',
+                        'pick',
+                        'text'
                     ]
                 ]
             ]);
