d282cbd766b9df73164e73bccb8388fe3556fa0a
bradyderoy <oR57$!^6yZbx>
2022-03-04 13:07:16 -0500

    refactor Rejects players that don't exist

--- app/Http/Controllers/Game/KickPlayerController.php
+++ app/Http/Controllers/Game/KickPlayerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Game;
 
 use App\Http\Controllers\Controller;
 use App\Models\Game;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class KickPlayerController extends Controller
@@ -14,7 +15,7 @@ class KickPlayerController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request, Game $game)
+    public function __invoke(Request $request, Game $game, User $user)
     {
         //
     }
--- tests/Feature/Http/Controllers/Game/KickPlayerControllerTest.php
+++ tests/Feature/Http/Controllers/Game/KickPlayerControllerTest.php
@@ -9,30 +9,36 @@ use Tests\TestCase;
 
 class KickPlayerControllerTest extends TestCase
 {
+    private $game;
     protected function setUp(): void
     {
         parent::setUp();
-
+        $this->game = Game::factory()->hasUsers(2)->create();
     }
 
     /** @test */
     public function it_will_not_allow_non_auth_users_to_kick_players()
     {
-        $game = Game::factory()->hasUsers(2)->create();
-        $player = $game->nonJudgeUsers()->first();
-        $this->postJson(route('api.game.player.kick', [$game->id, $player->id]))
+        $player = $this->game->nonJudgeUsers()->first();
+        $this->postJson(route('api.game.player.kick', [$this->game->id, $player->id]))
             ->assertUnauthorized();
     }
 
     /** @test */
     public function it_will_not_allow_auth_user_to_kick_a_player_invalid_game()
     {
-        $game = Game::factory()->hasUsers(2)->create();
-        $player = $game->nonJudgeUsers()->first();
-        $this->actingAs($game->judge)
+        $player = $this->game->nonJudgeUsers()->first();
+        $this->actingAs($this->game->judge)
             ->postJson(route('api.game.player.kick', [$this->faker->uuid, $player->id]))
             ->assertNotFound();
     }
 
+    /** @test */
+    public function it_will_reject_invalid_player_ids()
+    {
+        $this->actingAs($this->game->judge)
+            ->postJson(route('api.game.player.kick', [$this->game->id, 0]))
+            ->assertNotFound();
+    }
 
 }
