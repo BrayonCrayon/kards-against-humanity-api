e2bf71b24d15ed922efdca3c4c42f5c796756e52
bradyderoy <oR57$!^6yZbx>
2022-03-15 16:37:45 -0400

    This will not break anything

---
+++ tink.php
@@ -0,0 +1,22 @@
+<?php
+
+//use App\Models\Game;
+//use App\Models\User;
+//
+//$game = Game::factory()->create();
+//$user = User::factory()->create();
+//
+//\App\Events\GameJoined::dispatch($game, $user);
+// Not the judge
+// every other player's submitted data
+// Game Id passed through route
+
+
+$data = [
+    "user_id" => 1,
+    "submitted cards" => [
+        [
+            "id, text"
+        ]
+    ]
+];
