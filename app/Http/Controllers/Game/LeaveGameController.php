08c7548d0e0bb0dc1d1354e0655a9cc9c203ad6b
Brady Deroy <b.deroy@vehikl.com>
2022-09-13 18:06:09 -0400

    Allow a user to leave a game - refactor next judge code to a reusable function - allow a judge to leave a game - remove all left player cards - fix player redraw so that the cards go back into the deck - Allow an existing user to join an existing game

---
+++ app/Http/Controllers/Game/LeaveGameController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Game;
+
+use App\Models\Game;
+use App\Services\GameService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class LeaveGameController
+{
+    public function __invoke(Request $request, Game $game, GameService $service): JsonResponse
+    {
+        if (auth()->user()->id === $game->judge_id) {
+            $nextJudge = $service->nextJudge($game);
+            $service->updateJudge($game, $nextJudge->id);
+        }
+
+        $game->users()->detach(auth()->user()->id);
+        auth()->user()->hand()->forceDelete();
+        return response()->json();
+    }
+}
