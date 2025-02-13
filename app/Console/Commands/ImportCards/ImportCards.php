49e1a4dde49ac4e64feea288f30e75886bf0ecb8
Brady Deroy <b.deroy@vehikl.com>
2025-02-13 17:01:58 -0500

    - Modify import cards to import specific files - Add more test coverage - Be able to accept and import multiple files within a directory
    Co-authored-by: Felipe Flor <hello@felipeflor.com>
    Co-authored-by: Thiago Locks <thiago@zira.com.br>
    Co-authored-by: Sergey Putilov <sergey98putilov@gmail.com>
    Co-authored-by: Daniel Arcanjo <danlpsarcanjo@gmail.com>
    Co-authored-by: Christian Griffin <christian.griffin@live.com>
    Co-authored-by: Joel Repta <j.repta@vehikl.com>

--- app/Console/Commands/ImportCards/ImportCards.php
+++ app/Console/Commands/ImportCards/ImportCards.php
@@ -6,60 +6,92 @@ use App\Models\BlackCard;
 use App\Models\Expansion;
 use App\Models\WhiteCard;
 use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use InvalidArgumentException;
+use PHPUnit\Runner\FileDoesNotExistException;
 
 class ImportCards extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'kah:import-cards';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
+    protected $signature = 'kah:import-cards {--file=} {--dir=}]';
     protected $description = 'Import all cards';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         parent::__construct();
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
     public function handle(): int
     {
         $startLoading = now();
-        $this->loadCards();
+
+        try {
+            $this->loadExpansions();
+        } catch (FileDoesNotExistException|InvalidArgumentException $e) {
+            $this->error($e->getMessage());
+            return 1;
+        }
+
         $endLoading = now();
         $this->info("Loading Deck: {$endLoading->diffInMilliseconds($startLoading)}}");
+
         return 0;
     }
 
-    private function loadCards()
+    private function loadExpansions(): void
     {
-        $cardData = $this->loadDecks();
-
-        Expansion::insert($cardData['expansions']);
-        WhiteCard::insert($cardData['white_cards']);
-        BlackCard::insert($cardData['black_cards']);
+        $this->getFileNames()->each(fn($fileName) => $this->loadCards($fileName));
     }
 
-    private function loadDecks()
+    private function loadCards(string $fileName): void
     {
-        $jsonString = file_get_contents(app_path('Console/Commands/ImportCards/card-data.json'));
+        $cardData = $this->loadDecks($fileName);
+        $expansion = Expansion::query()->create($cardData['expansion']);
 
-        return json_decode($jsonString, true);
+        $blackCards = collect($cardData['black_cards'])->map(function ($blackCard) use ($expansion) {
+            return [
+                ...$blackCard,
+                'expansion_id' => $expansion->id,
+            ];
+        })->toArray();
+
+        $whiteCards = collect($cardData['white_cards'])->map(function ($whiteCards) use ($expansion) {
+            return [
+                ...$whiteCards,
+                'expansion_id' => $expansion->id,
+            ];
+        })->toArray();
+
+        WhiteCard::query()->insert($whiteCards);
+        BlackCard::query()->insert($blackCards);
+    }
+
+    private function loadDecks(string $fileName): array
+    {
+        return json_decode(Storage::disk()->get($fileName), true);
+    }
+
+    public function getFileNames(): Collection
+    {
+        if(!$this->option('file') && !$this->option('dir')) {
+            throw new InvalidArgumentException('Either the --file or --dir option must be provided.');
+        }
+
+        return $this->validateFiles($this->option('file') ?? $this->option('dir'));
+    }
+
+    private function validateFiles(string $path): Collection
+    {
+        if (!Storage::disk('local')->exists($path)) {
+            throw new FileDoesNotExistException($path);
+        }
+
+        if(Storage::disk('local')->directoryExists($path)) {
+            return collect(Storage::disk('local')->allFiles($path))
+                ->filter(fn($file) => Str::endsWith($file, '.json'));
+        }
+
+        return collect([$path]);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,12 +3,14 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-    use DatabaseTransactions;
+//    use DatabaseTransactions;
+    use RefreshDatabase;
     use WithFaker;
 }
